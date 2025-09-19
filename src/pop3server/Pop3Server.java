e3208a64a5c3bf48f3b499ccc2309ee534e93c58
SOuellette22 <ouellettese@merrimack.edu>
2025-09-19 12:39:08 -0400

    Feat: Made sure the any build stuff works.

---
+++ src/pop3server/Pop3Server.java
@@ -0,0 +1,7 @@
+package pop3server;
+
+public class Pop3Server {
+    public static void main(String[] args) {
+        System.out.println("POP3 Server is running...");
+    }
+}
---
+++ src/smtpserver/SmtpServer.java
@@ -0,0 +1,7 @@
+package smtpserver;
+
+public class SmtpServer {
+    public static void main(String[] args) {
+        System.out.println("SMTP Server is running...");
+    }
+}
